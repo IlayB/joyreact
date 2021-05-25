f738942cb063d1d1800e5f8d718d0adfbd0c6956
IlayB <ilayb776@gmail.com>
2021-05-25 17:06:17 +0300

    Header sfw and nsfw switch

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,11 +1,27 @@
 import { FaHome } from "react-icons/fa";
+import "./Main.css";
 
 function Main() {
   return (
-    <div style={{ fontWeight: "bold", color: "#8b857b" }}>
-      <a href="/" style={{ textDecoration: "none", color: "inherit" }}>
-        <FaHome /> JoyReactor
-      </a>
+    <div class="header">
+      <div>
+        <a href="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <FaHome /> JoyReactor
+        </a>
+      </div>
+      <div class="contentSwitch">
+        sfw
+        <span
+          style={{
+            paddingInline: "15px",
+          }}
+        >
+          <label class="switch">
+            <input type="checkbox" /> <div></div>
+          </label>
+        </span>
+        nsfw
+      </div>
     </div>
   );
 }
