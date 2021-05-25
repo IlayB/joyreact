bf55c71eb2bb75f612e1223732b41ae816d134d8
IlayB <ilayb776@gmail.com>
2021-05-25 20:10:31 +0300

    Content Header Search and buttons

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,23 +1,45 @@
 import "./Content.css";
+import { FaSearch } from "react-icons/fa";
+import { IconContext } from "react-icons/";
 
 function Content() {
   return (
     <div class="top">
-      <a href="/" class="headLink">
-        <span class="topText">ЛЕНТА</span>
-      </a>
-      <a href="/" class="headLink">
-        <span class="topText">ОБСУЖДАЕМОЕ</span>
-      </a>
-      <a href="/" class="headLink">
-        <span class="topText">ЛЮДИ</span>
-      </a>
-      <a href="/" class="headLink">
-        <span class="topText">О ПРОЕКТЕ</span>
-      </a>
-      <a href="/" class="headLink">
-        <span class="topText">СЛУЧАЙНЫЙ ПОСТ</span>
-      </a>
+      <span>
+        <a href="/" class="headLink">
+          <span class="topText">ЛЕНТА</span>
+        </a>
+        <a href="/" class="headLink">
+          <span class="topText">ОБСУЖДАЕМОЕ</span>
+        </a>
+        <a href="/" class="headLink">
+          <span class="topText">ЛЮДИ</span>
+        </a>
+        <a href="/" class="headLink">
+          <span class="topText">О ПРОЕКТЕ</span>
+        </a>
+      </span>
+      <span>
+        <a href="/" class="headLink">
+          <span class="topTextRandom">СЛУЧАЙНЫЙ ПОСТ {">"}</span>
+        </a>
+      </span>
+      <div class="search">
+        <input
+          type="search"
+          id="search"
+          name="search"
+          class="searchInput"
+          placeholder="Поиск"
+        />
+        <button onclick="submit" class="searchIcon">
+          <IconContext.Provider value={{ color: "white" }}>
+            <div>
+              <FaSearch />
+            </div>
+          </IconContext.Provider>
+        </button>
+      </div>
     </div>
   );
 }
