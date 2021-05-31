8604b5ed8147348737af0f362b905925dac21fff
IlayB <ilayb776@gmail.com>
2021-05-31 16:43:30 +0300

    Side bar

--- src/Content/Content.js
+++ src/Content/Content.js
@@ -1,12 +1,18 @@
 import "./Content.scss";
 import Posts from "./Posts/Posts.js";
 import BottomInfo from "./BottomInfo/BottomInfo.js";
+import SideBar from "./SideBar/SideBar";
 
 function Content() {
   return (
-    <div class="whiteBox">
-      <Posts />
-      <BottomInfo />
+    <div>
+      <div class="whiteBox">
+        <div class="principal">
+          <Posts />
+          <SideBar />
+        </div>
+        <BottomInfo />
+      </div>
     </div>
   );
 }
---
+++ src/Content/SideBar/SideBar.js
@@ -0,0 +1,18 @@
+import "./SideBar.scss";
+
+function SideBar() {
+  return (
+    <div class="sideBar">
+      <div class="hello">
+        <div class="sideBarText">ПРИВЕТ!</div>
+        <div>Ты тролль, лжец и девственник?</div>
+        <button class="joinButton">ПРИСОЕДИНЯЙСЯ!</button>
+      </div>
+      <div>
+        <div class="sideBarText">ЮМОР</div>
+      </div>
+    </div>
+  );
+}
+
+export default SideBar;
